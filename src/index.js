b419501cc23de99df9bfba54509d7a8efa9a6a87
Brian <okellobrian18@gmail.com>
2025-03-22 19:14:20 +0300

    Display Cuties on the character bar.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+const displayCuties = () => {
+    const cuties = document.querySelector("#character-bar")
+
+    fetch("http://localhost:3000/characters", {
+        method: "GET",
+        headers: {
+            "Content-Type" : "Application/json",
+            "Accept" : "Application/json",
+        },
+    })
+    .then( (response) => response.json())
+    .then( (data) => {
+        console.log(data)
+        data.forEach( (cutie) => {
+            const cutieSpan = document.createElement("span"); 
+            cutieSpan.id = `cutie-${cutie.id}`;
+            cutieSpan.textContent = cutie.name
+
+            cuties.appendChild(cutieSpan);
+        }) 
+    })
+}
+
+displayCuties()
