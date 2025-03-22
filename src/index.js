c3f7ec1590db7533c4394f7a1906c29a49234239
Brian <okellobrian18@gmail.com>
2025-03-22 19:35:47 +0300

    Displaying clicked on cutie.

--- src/index.js
+++ src/index.js
@@ -18,6 +18,27 @@ const displayCuties = () => {
             cutieSpan.id = `cutie-${cutie.id}`;
             cutieSpan.textContent = cutie.name
 
+            cutieSpan.addEventListener("click", () => {
+                const cutieMainDisplay = document.querySelector(".characterInfo")
+
+                cutieMainDisplay.innerHTML = ''
+
+                cutieMainDisplay.innerHTML = `
+                    <p id="name">${cutie.name}</p>
+                    <img
+                        id="image"
+                        src=${cutie.image}
+                        alt=${cutie.name}
+                    />
+                    <h4>Total Votes: <span id="vote-count">${cutie.votes} Votes</span></h4>
+                    <form id="votes-form">
+                        <input type="text" placeholder="Enter Votes" id="votes" name="votes" />
+                        <input type="submit" value="Add Votes" />
+                    </form>
+                    <button id="reset-btn">Reset Votes</button>
+                `
+            })
+
             cuties.appendChild(cutieSpan);
         }) 
     })
