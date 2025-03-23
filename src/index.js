d5bbf7250dbc0b337410a5c8ccf092052dfd3fbc
Brian <okellobrian18@gmail.com>
2025-03-23 09:19:59 +0300

    Reset vote count to 0.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const displayCuties = () => {
 
     // Display names of the animals
 
-    const cuties = document.querySelector("#character-bar");
+    const cuties = document.querySelector("#character-bar")
 
     fetch("http://localhost:3000/characters", {
         method: "GET",
@@ -14,14 +14,16 @@ const displayCuties = () => {
     .then((response) => response.json())
     .then((data) => {
         data.forEach((cutie) => {
-            const cutieSpan = document.createElement("span");
-            cutieSpan.id = `cutie-${cutie.id}`;
-            cutieSpan.textContent = cutie.name;
+            const cutieSpan = document.createElement("span")
+            cutieSpan.id = `cutie-${cutie.id}`
+            cutieSpan.textContent = cutie.name
 
             //Display Main Cutie
 
             cutieSpan.addEventListener("click", () => {
-                const cutieMainDisplay = document.querySelector(".characterInfo");
+
+                const cutieMainDisplay = document.querySelector(".characterInfo")
+
                 cutieMainDisplay.innerHTML = `
                     <p id="name">${cutie.name}</p>
                     <img
@@ -31,21 +33,24 @@ const displayCuties = () => {
                     />
                     <h4>Total Votes: <span id="vote-count">${cutie.votes} Votes</span></h4>
                     <form id="votes-form">
-                        <input type="number" placeholder="Enter Votes" id="votes" name="votes" />
-                        <input type="submit" value="Add Votes" />
+                    <input type="number" placeholder="Enter Votes" id="votes" name="votes" />
+                    <input type="submit" value="Add Votes" />
                     </form>
                     <button id="reset-btn">Reset Votes</button>
-                `;
+                    `;
+                    
+                    // Form Action
+                    
+                    const cutiesForm = document.getElementById("votes-form")
+                    const voteCountDisplay = document.getElementById("vote-count")
+                    const resetCutie = document.getElementById("reset-btn")
+                    
+                    cutiesForm.addEventListener("submit", (event) => {
 
-                // Form Action
+                    event.preventDefault()
 
-                const cutiesForm = document.getElementById("votes-form");
-                const voteCountDisplay = document.getElementById("vote-count");
-
-                cutiesForm.addEventListener("submit", (event) => {
-                    event.preventDefault();
-                    const voteInput = Number(document.getElementById("votes").value);
-                    const updatedVotes = Number(cutie.votes) + voteInput;
+                    const voteInput = Number(document.getElementById("votes").value)
+                    const updatedVotes = Number(cutie.votes) + voteInput
 
                     fetch(`http://localhost:3000/characters/${cutie.id}`, {
                         method: "PATCH",
@@ -57,16 +62,34 @@ const displayCuties = () => {
                     })
                     .then(resp => resp.json())
                     .then((updatedCutie) => {
-                        cutie.votes = Number(updatedCutie.votes); 
+                        cutie.votes = Number(updatedCutie.votes)
+                        voteCountDisplay.textContent = `${cutie.votes} Votes`
+                    })
+                })
+                
+                // Reset Vote count to 0
+
+                resetCutie.addEventListener("click", () => {
+                    fetch(`http://localhost:3000/characters/${cutie.id}`, {
+                        method: "PATCH",
+                        headers: {
+                            "Content-Type" : "Application/json",
+                            "Accept" : "Application/json"
+                        },
+                        body: JSON.stringify({ votes: 0 })
+                    })
+                    .then( resp => resp.json())
+                    .then(() => {
+                        cutie.votes = 0;
                         voteCountDisplay.textContent = `${cutie.votes} Votes`;
-                    });
-                });
-            });
+                      })
+                })
+            })
 
-            cuties.appendChild(cutieSpan);
-        });
+            cuties.appendChild(cutieSpan)
+        })
     })
-    .catch(error => console.error("Failed to fetch characters:", error));
-};
+    .catch(error => console.error("Failed to fetch characters:", error))
+}
 
-displayCuties();
+displayCuties()
